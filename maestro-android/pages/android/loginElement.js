1458c7146f0affcfe46cb0092b5d5ec9227c5f22
ivanspec <44164427+ivanspec@users.noreply.github.com>
2025-03-09 15:42:04 +0700

    feat: Init maestro (#3)
    * feat: init readm.md maestro
    
    * feat: add depedency and scenario login
    
    * chore: move test-demo.apk to apk folder
    
    * feat: add script start.sh
    
    * chore: update readme.md

---
+++ maestro-android/pages/android/loginElement.js
@@ -0,0 +1,11 @@
+output.login = {
+    menuLogin: 'Login',
+    titleLoginSignUpForm : 'Login / Sign up Form',
+    fieldEmail: 'Email',
+    fieldPwd: 'input-password',
+    descriptionLogin: 'When the device has Touch/FaceID (iOS) or FingerPrint enabled a biometrics button will be shown to use and test the login.',
+    btnLogin: 'button-LOGIN',
+    allertTitle: 'android:id/alertTitle',
+    allertMessage: 'android:id/message',
+    allertButton: 'android:id/button1',
+}
