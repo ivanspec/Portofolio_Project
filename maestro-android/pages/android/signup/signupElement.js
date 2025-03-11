cbee9b31567311772521c02ca0b7ada9ee549515
ivanspec <44164427+ivanspec@users.noreply.github.com>
2025-03-11 12:02:46 +0700

    feat[maestro]: add scenario signUp

---
+++ maestro-android/pages/android/signup/signupElement.js
@@ -0,0 +1,14 @@
+output.signup = {
+    menuLogin: 'Login',
+    titleLoginSignUpForm : 'Login / Sign up Form',
+    sectionSignUp: 'Sign up',
+    fieldEmail: 'input-email',
+    fieldPwd: 'input-password',
+    fieldRePwd: 'input-repeat-password',
+    descriptionLogin: 'When the device has Touch/FaceID (iOS) or FingerPrint enabled a biometrics button will be shown to use and test the login.',
+    btnLogin: 'button-LOGIN',
+    btnSignUp: 'SIGN UP',
+    allertTitle: 'android:id/alertTitle',
+    allertMessage: 'android:id/message',
+    allertButton: 'android:id/button1',
+}
